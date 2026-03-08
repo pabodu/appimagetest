e917074900b24b036583fc1f9e8bfd5a26ee8e88
pbd <pbd@s1.lan>
2026-03-08 23:51:42 +0500

    Simplified and corrected

--- create-appimage.sh
+++ create-appimage.sh
@@ -17,6 +17,7 @@ fi
 
 function message
 {
+    echo
     echo -e "\e[93m${1}\e[m"
     return 0
 }
@@ -30,7 +31,7 @@ function umount_dir
     done &&\
     return 0
 }
-message "Setting up building container..."
+message "Setting up build container..."
 apt-get update >/dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get -y install vim.tiny dialog fuse3 file make bubblewrap >/dev/null 2>&1
 mkdir /overlay && mount -t tmpfs -o size=${OVERLAY_TMPFS_SIZE} tmpfs /overlay
@@ -38,55 +39,43 @@ mkdir /overlay/{base,package,work,merged}
 message "Creating base..."
 cd "${PACKAGE}"
 while true; do
-    if [ -f overrides/set_up_base ]; then
-        function set_up_base
-        {
-            source overrides/set_up_base
-        }
-    else
+    if [ ! -f overrides/set_up_base ]; then
+        # This could go into overrides/set_up_base
+        #DEBIAN_FRONTEND=noninteractive apt-get install -y debootstrap >/dev/null 2>&1
+        #debootstrap --variant=minbase trixie /overlay/base >/dev/null 2>&1
 
-        function set_up_base
-        {
-        #    DEBIAN_FRONTEND=noninteractive apt-get install -y debootstrap >/dev/null 2>&1 &&\
-        #    debootstrap --variant=minbase trixie /overlay/base >/dev/null 2>&1 && SUFFIX="d"
-            mount -o bind,ro / /overlay/base
-        }
+        echo "mount -o bind,ro / /overlay/base" > overrides/set_up_base
     fi
 
     [ -n "$AUTOMATIC" ] && break
 
-    declare -f set_up_base
+    echo "set_up_base:"
+    cat overrides/set_up_base
     read -p "Is it what you want? [y/n] " reply
     [ "$reply" == "y" ] && break
     message "Running subshell..."
-    export -f set_up_base
     /bin/bash --rcfile <(echo "PS1='set_up_base> '") -i
 done
-set_up_base
+[ -n "$AUTOMATIC" ] && echo "Using set_up_base:" && cat overrides/set_up_base
+source overrides/set_up_base
 mount -t overlay overlay -o userxattr,lowerdir=/overlay/base,upperdir=/overlay/package,workdir=/overlay/work /overlay/merged
 mount --rbind /dev /overlay/merged/dev
 mount --rbind /dev/pts /overlay/merged/dev/pts
 mount --rbind /run /overlay/merged/run
 mount --bind /proc /overlay/merged/proc
 mount -t tmpfs tmpfs /overlay/merged/tmp
+message "Starting main installation..."
 cd "${PACKAGE}"
+mkdir /overlay/merged/package
+mount --bind "${PACKAGE}" /overlay/merged/package
 while true; do
-    if [ -f overrides/install ]; then
-        function install
-        {
-            source overrides/install
-        }
-    else
-        function install
-        {
-            return
-        }
-    fi
+    [ ! -f overrides/install ] && touch overrides/install
 
     [ -n "$AUTOMATIC" ] && break
 
-    declare -f install
-    read -p "Is overrides/install file ready? [y/n] " reply
+    echo "install:"
+    cat overrides/install
+    read -p "Is it what you want? [y/n] " reply
     [ "$reply" == "y" ] && break
     message "Running shell in /overlay/merged..."
     chroot /overlay/merged /bin/bash --rcfile <(echo "PROMPT_COMMAND='history -a'"; echo "PS1='\u@appimage \w\$ '") -i
@@ -97,15 +86,17 @@ while true; do
         [ "$reply" == "y" ] && cat /overlay/merged/root/.bash_history >> overrides/install
     fi
     message "Running subshell..."
-    export -f install
     /bin/bash --rcfile <(echo "PS1='install> '") -i
 done
+[ -n "$AUTOMATIC" ] && echo "Using install:" && cat overrides/install
+chroot /overlay/merged /bin/bash /package/overrides/install
 umount_dir /overlay/merged/proc
 umount_dir /overlay/merged/dev/pts
 umount_dir /overlay/merged/dev
 umount_dir /overlay/merged/run
 umount_dir /overlay/merged/tmp
 message "Package contents are in /overlay/merged and /overlay/package now"
+message "Need to find out which one to use..."
 cd "${PACKAGE}"
 while true; do
     if [ -f overrides/APPDIR ]; then
@@ -128,54 +119,53 @@ while true; do
 
     [ -n "$AUTOMATIC" ] && break
 
-    echo "APPDIR is set to $(cat overrides/APPDIR)"
+    echo -n "APPDIR: "; cat overrides/APPDIR
     read -p "Is it what you want? [y/n] " reply
     [ "$reply" == "y" ] && break
     rm -f overrides/APPDIR
 done
+[ -n "$AUTOMATIC" ] && echo "Using APPDIR:" && cat overrides/APPDIR
 message "Cleaning up..."
 cd "${PACKAGE}"
 while true; do
-    if [ -f overrides/cleanup ]; then
-        function cleanup
-        {
-            source overrides/cleanup
-        }
-    else
-        function cleanup
-        {
-            cd /overlay/merged
-            rm -rf \
-            root \
-            etc/ld.so.cache \
-            usr/share/doc \
-            usr/share/lintian \
-            var/log \
-            var/cache \
-            var/lib/dpkg \
-            var/lib/apt
-        }
+    if [ ! -f overrides/cleanup ]; then
+cat <<'OUTER' > overrides/cleanup
+cat <<EOF | xargs rm -rf
+root
+etc/ld.so.cache
+usr/share/doc
+usr/share/lintian
+var/log
+var/cache
+var/lib/dpkg
+var/lib/apt
+EOF
+OUTER
     fi
 
     [ -n "$AUTOMATIC" ] && break
 
-    declare -f cleanup
+    echo "cleanup:"
+    cat overrides/cleanup
     read -p "Is it what you want? [y/n] " reply
     [ "$reply" == "y" ] && break
     message "Running subshell..."
-    export -f cleanup
     /bin/bash --rcfile <(echo "PS1='cleanup> '") -i
 done
-cleanup
+[ -n "$AUTOMATIC" ] && echo "Using cleanup:" && cat overrides/cleanup
+cd /overlay/merged
+source package/overrides/cleanup
 cd "${PACKAGE}"
 if [ "$APPDIR" == "/overlay/merged" ]; then
     if [ -n "$AUTOMATIC" ]; then
         APPDIR="$APPDIR" make
     else
-        /bin/bash --rcfile <(echo "PS1='finalize (merged)> '") -i
+        APPDIR="$APPDIR" /bin/bash --rcfile <(echo "PS1='finalize (merged)> '") -i
     fi
 fi
 cd "${PACKAGE}"
+umount /overlay/merged/package
+rmdir /overlay/merged/package
 umount_dir /overlay/merged
 message "Package contents are in /overlay/package now"
 find /overlay/package -type c -exec rm -rf {} \;
@@ -183,7 +173,7 @@ if [ "$APPDIR" == "/overlay/package" ]; then
     if [ -n "$AUTOMATIC" ]; then
         APPDIR="$APPDIR" make
     else
-        bin/bash --rcfile <(echo "PS1='finalize (package)> '") -i
+        APPDIR="$APPDIR" /bin/bash --rcfile <(echo "PS1='finalize (package)> '") -i
     fi
 fi
 umount_dir /overlay
